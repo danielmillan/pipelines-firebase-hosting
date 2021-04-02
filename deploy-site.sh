4d802ecc364a744a41e90e8207635f81e9ad5b4b
Daniel F Millan <dani.millan07@gmail.com>
2021-04-02 16:23:24 -0500

    Add var to manage folder to deploy

--- deploy-site.sh
+++ deploy-site.sh
@@ -10,6 +10,9 @@ for arg in "$@"; do
     --project-name=*)
         PROJECT_NAME="${arg#*=}"
         ;;
+    --resources-name=*)
+        RESOURCES_NAME="${arg#*=}"
+        ;;
     esac
     shift
 done
@@ -18,10 +21,12 @@ showValidation() {
     echo "Make sure you are passing all required parameters:
         deploy-site.sh \\
             --firebase-token=(required) \\
+            --resources-name=(required) \\
             --project-name=(required)
         
         You currently passed:
         --firebase-token: $FIREBASE_TOKEN
+        --resources-name: $RESOURCES_NAME
         --project-name: $PROJECT_NAME
     " 1>&2
 
@@ -29,7 +34,7 @@ showValidation() {
 }
 
 # Validate params
-if [ -z "$FIREBASE_TOKEN" ] || [ -z "$PROJECT_NAME" ]; then
+if [ -z "$FIREBASE_TOKEN" ] || [ -z "$RESOURCES_NAME" ] || [ -z "$PROJECT_NAME" ]; then
     showValidation
 fi
 
