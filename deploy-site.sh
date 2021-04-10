c687edb6ef26e168e213ec5d056a600ef9b8f010
Daniel F Millan <dmilla3@bancodebogota.com.co>
2021-04-09 20:02:52 -0500

    Add POC commands

--- deploy-site.sh
+++ deploy-site.sh
@@ -42,7 +42,8 @@ fi
 npm ci
 sed -i -e "s#NAME_DIR#$RESOURCES_NAME#g" ./firebase.json
 sed -i -e "s#PROJECT#$PROJECT_NAME#g" ./.firebaserc
-# Create a target
+pwd
+ls -la
 echo "#### Deploying the site $PROJECT_NAME ####"
 # Deploy site in firebase
 ./node_modules/.bin/firebase deploy --token="$FIREBASE_TOKEN" --only hosting
