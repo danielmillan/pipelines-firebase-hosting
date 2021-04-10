70eb9e76aef9da4bf7accf2f1924c14cbb82a00c
Daniel F Millan <dmilla3@bancodebogota.com.co>
2021-04-09 20:25:41 -0500

    restructure path files

--- deploy-site.sh
+++ deploy-site.sh
@@ -40,8 +40,8 @@ fi
 
 # Install packages
 npm ci
-pwd
-ls -la ./.github/actions/pipelines-firebase-hosting
+cp ./.github/actions/pipelines-firebase-hosting/.firebaserc ./
+cp ./.github/actions/pipelines-firebase-hosting/.firebas.json ./
 sed -i -e "s#NAME_DIR#$RESOURCES_NAME#g" ./firebase.json
 sed -i -e "s#PROJECT#$PROJECT_NAME#g" ./.firebaserc
 echo "#### Deploying the site $PROJECT_NAME ####"
